69e194bae507b610729f09b2c59aabb9b437ca9b
georgeperry1 <gmperry18@gmail.com>
2018-03-02 23:17:51 +0100

    feat: added create vault pop-out window

--- src/app/Components/CreateVaultComponent.js
+++ src/app/Components/CreateVaultComponent.js
@@ -1,21 +1,37 @@
 'use strict';
 
 import { connect } from 'react-redux';
+import { CreateVaultWindowComponent } from  '../Components/CreateVaultWindowComponent';
 import React from 'react';
 import * as VaultActions from '../Actions/VaultActions';
 import '../Styles/CreateVaultComponent.css';
 
 class CreateVaultComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showWindow: false
+    };
+  }
 
-  createNewVault() {
-
+  showCreateNewVaultWindow() {
+    this.setState({
+      showWindow:!this.state.showWindow
+    });
+    //Send signal to main component
   }
 
   render() {
     return (
       <div className='create-vault-button-container'>
         <div className='col col-2 cv'>
-          <button className='create-vault-button'>Create Vault</button>
+          <button
+            className='create-vault-button'
+            onClick={() => this.showCreateNewVaultWindow()}
+          >
+            Create Vault
+          </button>
+          {this.state.showWindow ? <CreateVaultWindowComponent /> : null}
         </div>
       </div>
     )
---
+++ src/app/Components/CreateVaultWindowComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../Styles/CreateVaultWindowComponent.css';
+
+
+export const CreateVaultWindowComponent = (props) => (
+  <div className='create-vault-window'>
+    <div className='create-vault-window-inner'>
+      <h1>Hello</h1>
+    </div>
+  </div>
+)
