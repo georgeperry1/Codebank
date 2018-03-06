75ede74d025fc594d8046c75536593cd0af59bad
georgeperry1 <gmperry18@gmail.com>
2018-03-06 13:27:03 +0100

    feat: added GemListComponent & CSS

---
+++ src/app/Components/GemListComponent.js
@@ -0,0 +1,14 @@
+'use strict'
+
+import React from 'react';
+import '../Styles/GemListComponent.css';
+
+export class GemListComponent extends React.Component {
+  render() {
+    return (
+      <div className='gem-list-item'>
+        <h5>Gem List Component</h5>
+      </div>
+    )
+  }
+}
